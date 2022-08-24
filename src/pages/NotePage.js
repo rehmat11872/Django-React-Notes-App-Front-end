d2fced0c2a9edf13973925e305660fc5c2b63e63
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-08-24 13:32:44 +0000

    Note Updated

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 import { Link } from 'react-router-dom'
+import {  useNavigate } from "react-router-dom";
 
 function NotePage() {
     let params = useParams()
+    const navigate = useNavigate()
 
     let noteId = params.id
     console.log(noteId, "Note ID")
@@ -22,18 +24,40 @@ function NotePage() {
         setNote(data)
     }
 
+    let updateNote = async () => {
+        fetch(`/api/notes/${noteId}/update/`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(note)
+        })
+        
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        navigate(`/`)
+    }
+
+
+
+    let handleChange = (value) => {
+        setNote(note => ({ ...note, 'body': value }))
+        console.log('Handle Change:', note)
+    }
+
      
   return (
     
     <div className="note" >
       <div className="note-header">
         <h3>
-            <ArrowLeft  />
+            <ArrowLeft onClick={handleSubmit} />
         </h3>
       </div> 
 
-    <textarea  defaultValue={note?.body}></textarea>
-    
+      <textarea onChange={(e) => { handleChange(e.target.value) }} value={note?.body}></textarea>
     </div>
   )
 }
